dbffab4373a22eea0f07f7e5fde419a641fa7839
Saurav Shriwastav <42442539+sauravsvt@users.noreply.github.com>
2019-11-11 02:26:15 +0530

    Delete currency exchange rate.js

--- currency exchange rate.js
+++
@@ -1,3 +0,0 @@
-<div id="xecurrencywidget"></div>
-<script>var xeCurrencyWidget = {"domain":"sauravsvt.github.io/index.html","language":"en","size":"normal"};</script>
-<script src="https://www.xe.com/syndication/currencyconverterwidget.js"></script>
